41eae2c829b7f617453848cc3d31c399f8035813
CSU21-AINF <CSU21@academico.unab.ad>
2022-10-19 17:31:56 -0500

    Mi primer proyecto en python

---
+++ Fc barcelona.py
@@ -0,0 +1,17 @@
+#Clase
+class Fcbarcelona:
+    def __init__(self, fundacion, colores,estadio,presidente,ciudad):
+        self.fundacion = fundacion
+        self.colores = colores
+        self.estadio = estadio
+        self.presidente = presidente
+        self.ciudad = ciudad
+
+
+
+p1 = Fcbarcelona("1899","Azul y Rojo","Camp nou","Joan Laporta","Barcelona")
+print(p1.ciudad)                 
+
+
+
+                   
